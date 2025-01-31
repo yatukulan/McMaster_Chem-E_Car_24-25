448a1dcc68c5cf7cc38ecdb52fda774f6149dff6
Shyavan Sridhar <35411334+ShyavanS@users.noreply.github.com>
2025-01-31 16:41:23 -0500

    made updates for this year's circuitry setup.

--- Car_Code/cheme_car_datalogging_codebase/cheme_car_datalogging_codebase.ino
+++ Car_Code/cheme_car_datalogging_codebase/cheme_car_datalogging_codebase.ino
@@ -1,10 +1,8 @@
 // Included libraries
 #include <OneWire.h>
-#include <Wire.h>
-#include <MPU6050_light.h>
+// #include <Wire.h>
+// #include <MPU6050_light.h>
 #include <DallasTemperature.h>
-// #include "DFRobot_EC10.h"
-// #include <EEPROM.h>
 // #include <PID_v1_bc.h>
 #include <Servo.h>
 #include <Adafruit_NeoPixel.h>
@@ -13,157 +11,152 @@
 #define NUM_PIXELS 1 // Status LED
 
 // Define drive motor pins
-#define right_pwm1 10
-#define right_pwm2 9
-#define left_pwm1 12
-#define left_pwm2 11
+#define LEFT_PWM_1 9
+#define LEFT_PWM_2 10
+#define RIGHT_PWM_1 12
+#define RIGHT_PWM_2 11
 
-// Define the PWM pins for the stir bar motor
-#define stirPin1 24
-#define stirPin2 A3
+// Define the PWM pins for the stir bar motors
+#define BRAK_STIR_PWM_1 A3
+#define BRAK_STIR_PWM_2 24
+#define PROP_STIR_PWM_1 6
+#define PROP_STIR_PWM_2 5
 
-// Define servo pin
-#define servo_pwm 13
+// Define servo pins
+#define BRAK_SERVO_PWM 13
+#define PROP_SERVO_PWM 4
 
-#define ONE_WIRE_BUS A1 // pin for the DS18B20 data line
+#define BRAK_TEMP_SENS A1 // Pin for the teperature sensor data line
 
 // Define chip select pin for SD card
 #define SD_CS_PIN 23
 
-// #define EC_Pin A2 // Pin for conductivitiy probe
+// Create servo objects
+Servo brak_servo;
+Servo prop_servo;
 
-// #define EC_THRESH 100
+// MPU6050 mpu(Wire); // Create MPU6050 instance
 
-Servo servo; // Create servo object
-
-MPU6050 mpu(Wire); // Create MPU6050 instance
-
-OneWire oneWire(ONE_WIRE_BUS);       // Create a OneWire instance to communicate with the sensor
-DallasTemperature sensors(&oneWire); // Pass oneWire reference to Dallas Temperature sensor
+OneWire one_wire(BRAK_TEMP_SENS);          // Create a OneWire instance to communicate with the sensor
+DallasTemperature temp_sensors(&one_wire); // Pass OneWire reference to Dallas Temperature sensor
 
 Adafruit_NeoPixel pixel(NUM_PIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800); // Status LED
 
-// DFRobot_EC10 ec; // Create and instance for the conductivity probe
-
 // Define files
-SdFat SD;
+SdFat sd;
 File32 root;
-File32 nextFile;
-File32 dataFile;
+File32 next_file;
+File32 data_file;
 SdSpiConfig config(SD_CS_PIN, DEDICATED_SPI, SD_SCK_MHZ(16), &SPI1);
-String fileName;
+String file_name;
 
-bool isFileNew = true; // Checks for new file
+bool is_file_new = true; // Checks for new file
 
 // The target angle to keep car straight
-double goalAngle = 0.0;
+// double goal_angle = 0.0;
 
 // Define accelerometer variables
-double zAngle; // z-axis angle
+// double z_angle; // z-axis angle
 
 // Temperature threshold
-float tempDiff;
-
-// Initialize run count for SD card file
-int runCount;
+float temp_diff;
 
 // Temperature change
-float tempChange;
+float temp_change;
+
+// Initialize run count for SD card file
+int run_count;
 
 // variables to store temperature
-double temperatureC; // Current temperature
-double initTemp;     // Initial temperature for differential calculation
-
-// Variables for conductivity probe
-// double voltage;
-// double ecValue;
-
-double data[4]; // Data array
+double temperature_c; // Current temperature
+double init_temp;     // Initial temperature for differential calculation
 
 // KALMAN FILTER variables
 double x_temp; // Filtered temperature
 double p_temp; // Initial error covariance
-double x_MPU;  // Filtered temperature
-double p_MPU;  // Initial error covariance
+// double x_MPU;  // Filtered temperature
+// double p_MPU;  // Initial error covariance
 
 // Process noise and measurement noise
 double q_temp; // Process noise covariance
 double r_temp; // Measurement noise covariance
-double q_MPU;  // Process noise covariance
-double r_MPU;  // Measurement noise covariance
+// double q_MPU;  // Process noise covariance
+// double r_MPU;  // Measurement noise covariance
 
 // Keeping track of time
-float currTime = 0;
-unsigned long startTime;
-bool firstRun = true;
+float curr_time = 0;
+unsigned long start_time;
+bool first_run = true;
+
+int data_size = 2;      // Number of items to log
+double data[data_size]; // Data array
 
 // PID Loop variables
-// double pidOutput; // The output correction from the PID algorithm
+// double pid_output; // The output correction from the PID algorithm
 
 // The following numbers need to be adjusted through testing
-// double Kp = 1; // Proportional weighting
-// double Ki = 0; // Integral weighting
-// double Kd = 0; // Derivative weighting
+// double k_p = 1; // Proportional weighting
+// double k_i = 0; // Integral weighting
+// double k_d = 0; // Derivative weighting
 
 // Offset speeds for left and right wheel
 // int left_offset = 0;
 // int right_offset = 0;
 
 // PID control object; input, output, and goal angle are passed by pointer.
-// PID carPID(&x_MPU, &pidOutput, &goalAngle, Kp, Ki, Kd, DIRECT);
+// PID car_pid(&x_MPU, &pid_output, &goal_angle, k_p, k_i, k_d, DIRECT);
 
 void drive_forward(int speed) // Drive function
 {
-  // Right wheel
-  digitalWrite(right_pwm2, HIGH);
-  analogWrite(right_pwm1, speed);
-  // analogWrite(right_pwm1, speed - right_offset);
-
   // Left wheel
-  digitalWrite(left_pwm2, HIGH);
-  analogWrite(left_pwm1, speed);
-  // analogWrite(left_pwm1, speed - left_offset);
+  digitalWrite(LEFT_PWM_1, HIGH);
+  analogWrite(LEFT_PWM_2, speed);
+  // analogWrite(LEFT_PWM_2, speed - right_offset);
+
+  // Right wheel
+  digitalWrite(RIGHT_PWM_2, HIGH);
+  analogWrite(RIGHT_PWM_1, speed);
+  // analogWrite(RIGHT_PWM_1, speed - left_offset);
 }
 
 void stop_driving() // Stop function
 {
-  // Right wheel
-  digitalWrite(right_pwm2, HIGH);
-  analogWrite(right_pwm1, 255);
-
   // Left wheel
-  digitalWrite(left_pwm2, HIGH);
-  analogWrite(left_pwm1, 255);
+  digitalWrite(LEFT_PWM_1, HIGH);
+  analogWrite(LEFT_PWM_2, 255);
+
+  // Right wheel
+  digitalWrite(RIGHT_PWM_2, HIGH);
+  analogWrite(RIGHT_PWM_1, 255);
 }
 
-void servo_dump() // Dump contents of bowl into braking vessel with servo
+void servo_dump(Servo servo, int angle_us, int delay_ms) // Dump reactants into vessel with servo
 {
-  servo.writeMicroseconds(2600); // Rotate to 180 deg position without delay
-  delay(1000);                   // Wait 1 s
-  servo.writeMicroseconds(500);  // Return to default position
+  servo.writeMicroseconds(angle_us); // Rotate to specified position without delay
+  delay(delay_ms);                   // Wait specified delay
+  servo.writeMicroseconds(500);      // Return to default position
 }
 
-
-void start_stir() // Start stirring mechanism
+void start_stir(int stir_pin_1, int stir_pin_2, int speed) // Start stirring mechanism
 {
-  analogWrite(stirPin1, 255);  // 100% to overcome stall
-  digitalWrite(stirPin2, LOW); // For fast decay
-  delay(1000);                 // Wait 1 s to spin up
-  analogWrite(stirPin1, 146);  // 57% speed as per testing
+  digitalWrite(stir_pin_1, LOW);  // For fast decay
+  analogWrite(stir_pin_2, 255);   // 100% to overcome stall
+  delay(1000);                    // Wait 1 s to spin up
+  analogWrite(stir_pin_2, speed); // Set motor to speed obtained through testing
 }
 
 // void PID_loop() // Update motor speeds according to PID algorithm
 // {
-//   carPID.Compute(); // Run compute algorithm and updates pidOutput
+//   car_pid.Compute(); // Run compute algorithm and updates pid_output
 
-//   if (pidOutput > 0)
+//   if (pid_output > 0)
 //   {
-//     left_offset = abs(round(pidOutput)); // If output needs to be adjusted in positive dir (to the right), increase left wheel speed
+//     left_offset = abs(round(pid_output)); // If output needs to be adjusted in positive dir (to the right), increase left wheel speed
 //     right_offset = 0;                    // Zero other wheel offset to prevent instability
 //   }
-//   else if (pidOutput < 0)
+//   else if (pid_output < 0)
 //   {
-//     right_offset = abs(round(pidOutput)); // If output needs to be adjusted in negative dir (to the left), increase right wheel speed
+//     right_offset = abs(round(pid_output)); // If output needs to be adjusted in negative dir (to the left), increase right wheel speed
 //     left_offset = 0;                      // Zero other wheel offset to prevent instability
 //   }
 //   else
@@ -197,18 +190,18 @@ void kalman_filter(double x_k, double p_k, double q, double r, double input, boo
   }
   else
   {
-    x_MPU = x_k;
-    p_MPU = p_k;
+    // x_MPU = x_k;
+    // p_MPU = p_k;
   }
 }
 
-void printer(bool serialTrue, unsigned long millisTime, double outputs[2]) // Output function
+void printer(bool serial_true, unsigned long millis_time, double outputs[data_size]) // Output function
 {
-  if (serialTrue) // Print data to serial or SD card file accordingly in .csv format
+  if (serial_true) // Print data to serial or SD card file accordingly in .csv format
   {
-    Serial.print(millisTime);
+    Serial.print(millis_time);
 
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < data_size; i++)
     {
       Serial.print(",");
       Serial.print(outputs[i]);
@@ -218,24 +211,20 @@ void printer(bool serialTrue, unsigned long millisTime, double outputs[2]) // Ou
   }
   else
   {
-    dataFile.print(millisTime);
+    data_file.print(millis_time);
 
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < data_size; i++)
     {
-      dataFile.print(",");
-      dataFile.print(outputs[i]);
+      data_file.print(",");
+      data_file.print(outputs[i]);
     }
 
-    dataFile.println("");
+    data_file.println("");
   }
 }
 
 void setup() // Setup (executes once)
 {
-  // Tie pin 4 to GND for compatibility
-  pinMode(GND_PIN, OUTPUT);
-  digitalWrite(GND_PIN, LOW);
-
   // Indicate status to be initialized
   pixel.begin();
   pixel.setBrightness(255);
@@ -244,27 +233,27 @@ void setup() // Setup (executes once)
   pixel.show();
 
   // Get time at start
-  startTime = millis();
+  start_time = millis();
 
-  while (!SD.begin(config))
+  while (!sd.begin(config))
   {
     delay(1000); // Wait for a second before retrying
   }
 
-  root = SD.open("/", FILE_READ); // Open SD root directory
-  runCount = 0;
+  root = sd.open("/", FILE_READ); // Open SD root directory
+  run_count = 0;
 
   while (true)
   {
-    nextFile = root.openNextFile();
+    next_file = root.openNextFile();
 
-    if (nextFile)
+    if (next_file)
     {
-      runCount++; // Increment with each existing file
+      run_count++; // Increment with each existing file
     }
     else
     {
-      nextFile.close();
+      next_file.close();
       break;
     }
   }
@@ -272,60 +261,56 @@ void setup() // Setup (executes once)
   root.close();
 
   // Setting to drive motors output mode
-  pinMode(right_pwm1, OUTPUT);
-  pinMode(right_pwm2, OUTPUT);
-  pinMode(left_pwm1, OUTPUT);
-  pinMode(left_pwm2, OUTPUT);
+  pinMode(LEFT_PWM_1, OUTPUT);
+  pinMode(LEFT_PWM_2, OUTPUT);
+  pinMode(RIGHT_PWM_1, OUTPUT);
+  pinMode(RIGHT_PWM_2, OUTPUT);
 
-  stop_driving(); // Stop driving motors from residual bootloader code
-
-  // Initialize the stir motor pins as outputs
-  pinMode(stirPin1, OUTPUT);
-  pinMode(stirPin2, OUTPUT);
+  stop_driving(); // Stop driving motors from any residual bootloader code
 
   // Initialize the stir motor pins as outputs
-  pinMode(linAcc1, OUTPUT);
-  pinMode(linAcc2, OUTPUT);
+  pinMode(BRAK_STIR_PWM_1, OUTPUT);
+  pinMode(BRAK_STIR_PWM_2, OUTPUT);
+  pinMode(PROP_STIR_PWM_1, OUTPUT);
+  pinMode(PROP_STIR_PWM_2, OUTPUT);
 
   // Setting the stir speed
-  start_stir();
+  start_stir(BRAK_STIR_PWM_1, BRAK_STIR_PWM_2, 146);
+  start_stir(PROP_STIR_PWM_1, PROP_STIR_PWM_2, 146);
 
-  // Inject the contents of the syringe
-  inject();
+  temp_sensors.begin();                        // Initialize the DS18B20 sensor
+  temp_sensors.requestTemperatures();          // Request temperature from all devices on the bus
+  init_temp = temp_sensors.getTempCByIndex(0); // Get temperature in Celsius
 
-  // ec.begin();
-  sensors.begin();                       // Initialize the DS18B20 sensor
-  sensors.requestTemperatures();         // Request temperature from all devices on the bus
-  initTemp = sensors.getTempCByIndex(0); // Get temperature in Celsius
-
-  Wire.begin();             // Initialize I2C communication
-  mpu.begin();              // Initialize MPU6050
-  mpu.calcOffsets();        // Zero yaw angle
-  mpu.update();             // Update MPU readings
-  zAngle = mpu.getAngleZ(); // Get z-axis angle from MPU
+  // Wire.begin();             // Initialize I2C communication
+  // mpu.begin();              // Initialize MPU6050
+  // mpu.calcOffsets();        // Zero yaw angle
+  // mpu.update();             // Update MPU readings
+  // z_angle = mpu.getAngleZ(); // Get z-axis angle from MPU
 
   // Initialize Kalman filter parameters
-  x_temp = initTemp; // Initial state estimate
-  p_temp = 0.1;      // Initial error covariance
-  q_temp = 0.01;     // Process noise covariance
-  r_temp = 0.5;      // Measurement noise covariance
-  x_MPU = zAngle;    // Initial state estimate
-  p_MPU = 1.0;       // Initial error covariance
-  q_MPU = 0.01;      // Process noise covariance
-  r_MPU = 0.1;       // Measurement noise covariance
+  x_temp = init_temp; // Initial state estimate
+  p_temp = 0.1;       // Initial error covariance
+  q_temp = 0.01;      // Process noise covariance
+  r_temp = 0.5;       // Measurement noise covariance
+  // x_MPU = z_angle;    // Initial state estimate
+  // p_MPU = 1.0;       // Initial error covariance
+  // q_MPU = 0.01;      // Process noise covariance
+  // r_MPU = 0.1;       // Measurement noise covariance
 
   // Initialize servo to default position
-  servo.attach(servo_pwm, 500, 2600);
-
+  brak_servo.attach(BRAK_SERVO_PWM, 500, 2500);
+  prop_servo.attach(PROP_SERVO_PWM, 500, 2500);
 
   // Dump reactants before starting drive
-  servo_dump();
+  servo_dump(prop_servo, 2500, 1000);
+  servo_dump(brak_servo, 1500, 6000);
 
   // Activate PID
-  // carPID.SetMode(AUTOMATIC);
+  // car_pid.SetMode(AUTOMATIC);
 
   // The pid outputs between -51 to 51 depending on how the motors should be adjusted. An output of 0 means no change. (This should be adjusted through testing).
-  // carPID.SetOutputLimits(-51, 51);
+  // car_pid.SetOutputLimits(-51, 51);
 
   pixel.setPixelColor(0, 0, 0, 255); // Indicate setup complete status
   pixel.show();
@@ -336,65 +321,63 @@ void setup() // Setup (executes once)
 
 void loop() // Loop (main loop)
 {
-  sensors.requestTemperatures();             // Request temperature from all devices on the bus
-  temperatureC = sensors.getTempCByIndex(0); // Get temperature in Celsius
+  temp_sensors.requestTemperatures();              // Request temperature from all devices on the bus
+  temperature_c = temp_sensors.getTempCByIndex(0); // Get temperature in Celsius
 
-  // voltage = analogRead(EC_Pin)/1024.0*5000;
-  // ecValue = ec.readEC(voltage, temperatureC);
-
-  mpu.update();             // Update MPU readings
-  zAngle = mpu.getAngleZ(); // Get z-axis angle from MPU
+  // mpu.update();             // Update MPU readings
+  // z_angle = mpu.getAngleZ(); // Get z-axis angle from MPU
 
   // Update kalman filters
-  kalman_filter(x_temp, p_temp, q_temp, r_temp, temperatureC, true);
-  kalman_filter(x_MPU, p_MPU, q_MPU, r_MPU, zAngle, false);
+  kalman_filter(x_temp, p_temp, q_temp, r_temp, temperature_c, true);
+  // kalman_filter(x_MPU, p_MPU, q_MPU, r_MPU, z_angle, false);
 
   // Get time on each measurement of sensor
-  if (firstRun)
+  if (first_run)
   {
-    startTime = millis(); // First measurement saved seperately
-    currTime = 0;         // Set current time to zero from start time
-    firstRun = false;
+    start_time = millis(); // First measurement saved seperately
+    curr_time = 0;         // Set current time to zero from start time
+    first_run = false;
   }
   else
   {
-    currTime = (millis() - startTime) / 1000; // Taken to check time against first measurement
+    curr_time = (millis() - start_time) / 1000; // Taken to check time against first measurement
   }
 
   // Open csv file
-  fileName = "Run_" + String(runCount) + ".csv";
-  dataFile = SD.open(fileName, FILE_WRITE);
+  file_name = "Run_" + String(run_count) + ".csv";
+  data_file = sd.open(file_name, FILE_WRITE);
 
-  if (dataFile)
+  if (data_file)
   {
     // Writes header if it's a new file
-    if (isFileNew)
+    if (is_file_new)
     {
-      dataFile.println("Time,Temperature,Filtered Temperature");
-      isFileNew = false;
+      data_file.println("Time,Temperature,Filtered Temperature");
+      is_file_new = false;
     }
 
     // Update data array
-    data[0] = temperatureC;
+    data[0] = temperature_c;
     data[1] = x_temp;
-    data[2] = zAngle;
-    data[3] = x_MPU;
+    // data[2] = z_angle;
+    // data[3] = x_MPU;
 
     // Write variable data to the file in CSV format
-    printer(false, currTime, data);
+    printer(false, curr_time, data);
 
-    dataFile.close();
+    data_file.close();
   }
 
-  tempDiff = -0.068 * currTime + 1.4; // Update temperature differential
-  tempChange = x_temp - initTemp;     // Calculate temperature change  
+  temp_diff = -0.068 * curr_time + 1.4; // Update temperature differential
+  temp_change = x_temp - init_temp;     // Calculate temperature change
 
   drive_forward(128); // 50% speed in slow decay mode
 
   // // Update PID model
   // PID_loop();
 
-  if(tempChange >= tempDiff) {
+  if (temp_change >= temp_diff)
+  {
     // Stop driving
     stop_driving();
 
@@ -404,5 +387,5 @@ void loop() // Loop (main loop)
 
     while (1)
       ; // Do nothing for remainder of uptime
-  } 
+  }
 }
